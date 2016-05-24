1f8dee6938f0587877b96cbd2751fdf0a5a66e07
Ahmed Toumi <ahmed.ettoumi@gmail.com>
2016-05-24 17:28:23 +0200

    Update add_aliases.sh

--- add_aliases.sh
+++ add_aliases.sh
@@ -11,13 +11,14 @@ git_stash_and_get_branch () {
 # update the parent branch (param1), update the child branch (param2), than merge the first one on the second one
 # When conflict, you have to resolve manually those one, index them (git add .) than commit (git commit) to end the merge process
 git_update_branch1_from_branch2 () {
-    echo "start checkouting and up to date the branch : $1"
-    git checkout $1
-    git pull --rebase origin $1
     echo "start checkouting and up to date the branch : $2"
     git checkout $2
-    git pull --rebase origin NUMERIQUE-$2
-    git merge $1
+    git pull --rebase origin $2
+    echo "start checkouting and up to date the branch : $1"
+    git checkout $1
+    git pull --rebase origin NUMERIQUE-$1
+    echo "start merging $2 on $1"
+    git merge $2
 }
 ##   ALIAS    ##
 alias update_branch=git_update_branch1_from_branch2
